9340ee2cbc749d7ec126ff3fc8553acfc918e2be
arfa-29 <arfaasif1@gmail.com>
2025-05-09 16:43:55 +0500

    Add files via upload

---
+++ Number is even or odd.py
@@ -0,0 +1,9 @@
+
+# Check whether a number is Even or Odd
+
+my_number = int(input("Enter the number: "))
+
+if my_number % 2 == 0:
+    print(f"{my_number} is an Even number")
+else:
+    print(f"{my_number} is an Odd number")
---
+++ Two_numbers_calculation .py
@@ -0,0 +1,27 @@
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+def my_function (num1,num2):
+    add_num = num1 + num2
+    return add_num
+output = my_function (num1,num2)
+print(f"Addition of {num1} and {num2} is {output}")
+
+def my_function (num1,num2):
+    subtract_num = num1 - num2
+    return subtract_num
+output = my_function (num1,num2)
+print(f"Subtraction of {num1} and {num2} is {output}")
+
+def my_function (num1,num2):
+    multiply_num = num1 * num2
+    return multiply_num
+output = my_function (num1,num2)
+print(f"Multiplication of {num1} and {num2} is {output}")
+
+def my_function (num1,num2):
+    divie_num = num1 / num2
+    return divie_num
+output = my_function (num1,num2)
+print(f"Division of {num1} and {num2} is {output}")
